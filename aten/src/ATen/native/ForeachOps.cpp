96a8d1f76f1e7bf640bb29a97bed05d882b432ed
Iurii Zdebskyi <iuriiz@devfair004.maas>
2020-07-20 11:22:36 -0700

    Added some tests

--- aten/src/ATen/native/ForeachOps.cpp
+++ aten/src/ATen/native/ForeachOps.cpp
@@ -6,7 +6,10 @@ namespace at {
 namespace native {
 
 std::vector<Tensor> _foreach_add(TensorList tensors, Scalar scalar) {
-  
+  if (tensors.size() == 0) {
+    throw std::runtime_error("Tensor list can't be empty.");
+  }
+
   return foreach_tensor_add_scalar_stub(tensors[0].device().type(), tensors, scalar);
 }
 DEFINE_DISPATCH(foreach_tensor_add_scalar_stub);
